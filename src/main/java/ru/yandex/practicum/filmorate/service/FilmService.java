354427d09f7c9c7ac05fda9d9d745dfbc36ee069
Артём <Loksarr@yandex.ru>
2026-08-24 23:05:48 +0300

    Чексатйл

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -124,7 +124,6 @@ public class FilmService {
         log.info("Пользователь {} убрал лайк с фильма {}", userId, filmId);
     }
 
-
     public Collection<Film> searchFilms(String query, String by) {
 
         if (query == null || query.isBlank()) {
