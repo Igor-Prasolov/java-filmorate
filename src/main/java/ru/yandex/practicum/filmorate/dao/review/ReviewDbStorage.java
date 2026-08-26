f3be5c6c10d285f9d3d8f4f16f3ce8b51f41ed31
Igor <igorprasolov00259@gmail.com>
2026-08-26 05:23:29 +0300

    чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/dao/review/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/review/ReviewDbStorage.java
@@ -14,7 +14,6 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
